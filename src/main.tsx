cf1280b50a98bcb045f866a537926ebb49d1c3d7
AndyZhengyan <hzzhengyan@qq.com>
2026-06-01 16:02:40 +0800

    fix: declare settings to satisfy obsidian 1.13.0 stricter base property check

--- src/main.tsx
+++ src/main.tsx
@@ -79,7 +79,7 @@ function normalizeSyncHistory(value: unknown): SyncHistoryEntry[] {
 }
 
 export default class GetNoteSyncPlugin extends Plugin {
-  settings!: Settings;
+  declare settings: Settings;
   isSyncing = false;
   syncProgress: SyncProgressDetail = { message: '', count: '', percent: 0 };
   syncHistory: SyncHistoryEntry[] = [];
